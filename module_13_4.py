9bb6165166ac6100e803ca57c46337ea69c36145
Poleshook <poleshook@mail.ru>
2024-12-01 10:22:46 +0500

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,61 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text='Calories')
+async def set_age(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(first=message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(second=message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(third=message.text)
+    data = await state.get_data()
+    result = float(10 * float(data['third']) + 6.25 * float(data['second']) - 5 * float(data['first']) + 5)
+    await message.answer(f'Ваша норма калорий {result}')
+    state.finish()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    #print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.\n'
+                         'Что бы узнать Вашу суточную норму калорий, введите Calories')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    #print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
